7a492ad4896994fb5eea5c9c008b9ce895cc4206
akmaalsyh <taufansyah50@gmail.com>
2024-03-26 15:11:28 +0700

    membuat do while switch case

--- 236_FungsiSwitch.cpp
+++ 236_FungsiSwitch.cpp
@@ -35,3 +35,43 @@ float pembagian(float a, float b)
     
 }
 
+int main()
+{
+    int pilihan;
+
+    do{
+        cout << "Kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan : " ;
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+            case 1:
+                inputdata();
+                cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+                break;
+            case 2:
+                cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+                break;
+            case 3:
+                cout << "Hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+                break;
+            case 4:
+                cout << "Hasil pembagian = " << pembagian(bilangan1, bilangan2) << endl;
+                break;
+            case 5:
+                break;
+            
+            default:
+                cout << "Pilihan tidak ada" << endl;
+                break;
+        } 
+    } while (pilihan !=5);
+}
